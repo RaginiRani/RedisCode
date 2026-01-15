9d5e31065377292c0286d0f2dd176178d84ff180
Ragini R <raginirani214@gmail.com>
2026-01-15 17:47:06 +0530

    Sting DataType-set,get-nx,expire

---
+++ string.js
@@ -0,0 +1,12 @@
+const client = require('./client');
+
+async function init() {
+    //set
+    //await client.set("msg:6", "Hey from NodeJs");
+    await client.expire("msg:6", 10);
+    //get
+    const res = await client.get('msg:6'); 
+    //const res = await client.get('user:1'); 
+    console.log("Result->", res);
+}
+init();
